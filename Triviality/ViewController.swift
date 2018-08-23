3baacdcf3eb66817ee22518e90f0568e4ac07e15
Leeann Drees <leeann.drees@detroitlabs.com>
2018-08-23 15:33:12 -0400

    initial commit

---
+++ Triviality/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Triviality
+//
+//  Created by DetroitLabs on 8/23/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+/**App Challenge #9: Triviality*
+ 
+ Create a trivia game application consisting of at least 8 questions
+ If the user guesses a question correctly, the background of the app should flash green, and the user should be taken to the next question
+ If the user chooses an incorrect answer, the background of the app should flash red, and that answer should be disabled
+ If the user gets more than 3 questions wrong, the user should lose the game
+ At the end of the game, your app should show the user their score
+ The user should be able to start over when the game ends
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
